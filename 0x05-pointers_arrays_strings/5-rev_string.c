33551b0cf1c00dfca6f720a5688244df4ef48a49
Iwanefun Oluwatimilehin <crypto22122@gmail.com>
2023-09-18 13:48:15 -0700

    my work

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: the string
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char tmp;
+
+	i = _strlen(s);
+	j = 0;
+	while (j < (i / 2))
+	{
+		tmp = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = tmp;
+		j++;
+	}
+}
