74b6cad0f645ecd607ed133bd99119d52e4b2ad1
Iwanefun Oluwatimilehin <crypto22122@gmail.com>
2024-03-01 03:34:52 -0800

    my work

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - test function
+ *
+ * @c: test parameter
+ * Return: nothing
+ */
+int _isupper(int c)
+{
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - test function
+ *
+ * @s: test param
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+}
